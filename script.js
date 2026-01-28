40278c221e344c92f5996ad835b78843289fd3cb
Sanjeev Tiwari <sanjeevtiwari@gmail.com>
2026-01-28 18:02:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const inputBox = document.getElementById("input-box")
+const listcontainer = document.getElementById("list-container")
+
+
+function addtask(){
+    if (inputBox.value === ''){
+        alert("Add your task")
+    }
+
+
+else {
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement('span');
+    span.textContent = "×"
+    li.appendChild(span)
+
+    }
+
+    inputBox.value = "";
+    saveData()
+
+} 
+
+
+
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }
+    
+
+    else if(e.target.tagName === "SPAN"){
+      e.target.parentElement.remove()
+      saveData()
+     }
+    
+} , false ) 
+
+function saveData() {
+    localStorage.setItem("data" , listcontainer.innerHTML)
+}
+
+function showTask() {
+    listcontainer.innerHTML = localStorage.getItem("data")
+}
+
+
+showTask()
